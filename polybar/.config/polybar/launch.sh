2959194e5096bd9c29ea1ee6184bd0dcb7b2cfbb
Kamil Kosiba <kamil.kosiba@outlook.com>
2024-12-23 20:14:52 +0000

    Add polybar config

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar top 2>&1 | tee -a /tmp/polybar.log &
+disown
