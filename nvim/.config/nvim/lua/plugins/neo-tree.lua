2e01e20a0f9608986e894d3e518baefcf2d04307
Kamil Kosiba <kamil.kosiba@outlook.com>
2025-01-04 16:15:53 +0000

    Correctly hijack netrw in neo-tree plugin

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,8 +6,8 @@ return {
 				hide_dotfiles = false,
 				hide_gitignored = false,
 			},
+			-- Use neo-tree instead of netrw in expanded mode
+			hijack_netrw_behavior = "open_current",
 		},
-		-- Use neo-tree instead of netrw in expanded mode
-		hijack_netrw_behavior = "open_current",
 	},
 }
